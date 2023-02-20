aa8b74dfd05c6fc589427c6e9b36332b25ae3f9e
Koti <koti7216@gmail.com>
2023-02-19 19:23:04 -0500

    security

--- src/main/java/com/example/demo2/service/WebConfig.java
+++ src/main/java/com/example/demo2/service/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/user/4th").permitAll()
+                .antMatchers("/user/4th","/user/1st").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .oauth2ResourceServer()
@@ -41,7 +41,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     public void configure(WebSecurity webSecurity) throws Exception {
-        webSecurity.ignoring().antMatchers("/user/4th");}
+        webSecurity.ignoring().antMatchers("/user/4th","/user/1st");}
 
     public String tokenExtractor(HttpServletRequest request) {
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
